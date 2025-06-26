22ecb62b6aa81ec81b7525df067504b993cfdcc5
tenseite <bogdahnnikonov@yandex.ru>
2025-06-26 09:55:46 +0300

    Add files via upload

---
+++ ProgressBar/WindowsFormsApp2/FrmProgressBarSample.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class FrmProgressBarSample
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ProgressBarProcess = new System.Windows.Forms.ProgressBar();
+            this.LabelPercentComplete = new System.Windows.Forms.Label();
+            this.LabelPercentCompleteText = new System.Windows.Forms.Label();
+            this.LabelStatus = new System.Windows.Forms.Label();
+            this.LabelStatusText = new System.Windows.Forms.Label();
+            this.LabelProgressStep = new System.Windows.Forms.Label();
+            this.ButtonReset = new System.Windows.Forms.Button();
+            this.ButtonStop = new System.Windows.Forms.Button();
+            this.TimerForProgressBar = new System.Windows.Forms.Timer(this.components);
+            this.ComboBoxProgressStep = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // ProgressBarProcess
+            // 
+            this.ProgressBarProcess.Location = new System.Drawing.Point(12, 21);
+            this.ProgressBarProcess.Name = "ProgressBarProcess";
+            this.ProgressBarProcess.Size = new System.Drawing.Size(616, 23);
+            this.ProgressBarProcess.TabIndex = 0;
+            this.ProgressBarProcess.Click += new System.EventHandler(this.ProgressBarProcess_Click);
+            // 
+            // LabelPercentComplete
+            // 
+            this.LabelPercentComplete.AutoSize = true;
+            this.LabelPercentComplete.Location = new System.Drawing.Point(9, 47);
+            this.LabelPercentComplete.Name = "LabelPercentComplete";
+            this.LabelPercentComplete.Size = new System.Drawing.Size(67, 13);
+            this.LabelPercentComplete.TabIndex = 1;
+            this.LabelPercentComplete.Text = "Выполнено:";
+            this.LabelPercentComplete.Click += new System.EventHandler(this.LabelPercentComplete_Click);
+            // 
+            // LabelPercentCompleteText
+            // 
+            this.LabelPercentCompleteText.AutoSize = true;
+            this.LabelPercentCompleteText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelPercentCompleteText.Location = new System.Drawing.Point(82, 47);
+            this.LabelPercentCompleteText.Name = "LabelPercentCompleteText";
+            this.LabelPercentCompleteText.Size = new System.Drawing.Size(16, 13);
+            this.LabelPercentCompleteText.TabIndex = 2;
+            this.LabelPercentCompleteText.Text = "%";
+            this.LabelPercentCompleteText.Click += new System.EventHandler(this.LabelPercentCompleteText_Click);
+            // 
+            // LabelStatus
+            // 
+            this.LabelStatus.AutoSize = true;
+            this.LabelStatus.Location = new System.Drawing.Point(9, 69);
+            this.LabelStatus.Name = "LabelStatus";
+            this.LabelStatus.Size = new System.Drawing.Size(44, 13);
+            this.LabelStatus.TabIndex = 3;
+            this.LabelStatus.Text = "Статус:";
+            this.LabelStatus.Click += new System.EventHandler(this.LabelStatus_Click);
+            // 
+            // LabelStatusText
+            // 
+            this.LabelStatusText.AutoSize = true;
+            this.LabelStatusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelStatusText.Location = new System.Drawing.Point(82, 69);
+            this.LabelStatusText.Name = "LabelStatusText";
+            this.LabelStatusText.Size = new System.Drawing.Size(14, 13);
+            this.LabelStatusText.TabIndex = 4;
+            this.LabelStatusText.Text = "?";
+            this.LabelStatusText.Click += new System.EventHandler(this.LabelStatusText_Click);
+            // 
+            // LabelProgressStep
+            // 
+            this.LabelProgressStep.AutoSize = true;
+            this.LabelProgressStep.Location = new System.Drawing.Point(328, 111);
+            this.LabelProgressStep.Name = "LabelProgressStep";
+            this.LabelProgressStep.Size = new System.Drawing.Size(107, 13);
+            this.LabelProgressStep.TabIndex = 5;
+            this.LabelProgressStep.Text = "Шаг прогресс бара:";
+            // 
+            // ButtonReset
+            // 
+            this.ButtonReset.Location = new System.Drawing.Point(13, 106);
+            this.ButtonReset.Name = "ButtonReset";
+            this.ButtonReset.Size = new System.Drawing.Size(130, 23);
+            this.ButtonReset.TabIndex = 6;
+            this.ButtonReset.Text = "&Сброс";
+            this.ButtonReset.UseVisualStyleBackColor = true;
+            this.ButtonReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ButtonStop
+            // 
+            this.ButtonStop.Location = new System.Drawing.Point(149, 106);
+            this.ButtonStop.Name = "ButtonStop";
+            this.ButtonStop.Size = new System.Drawing.Size(130, 23);
+            this.ButtonStop.TabIndex = 7;
+            this.ButtonStop.Text = "&Остановить";
+            this.ButtonStop.UseVisualStyleBackColor = true;
+            this.ButtonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // TimerForProgressBar
+            // 
+            this.TimerForProgressBar.Interval = 1000;
+            this.TimerForProgressBar.Tick += new System.EventHandler(this.TimerForProgressBar_Tick);
+            // 
+            // ComboBoxProgressStep
+            // 
+            this.ComboBoxProgressStep.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxProgressStep.FormattingEnabled = true;
+            this.ComboBoxProgressStep.Location = new System.Drawing.Point(441, 108);
+            this.ComboBoxProgressStep.Name = "ComboBoxProgressStep";
+            this.ComboBoxProgressStep.Size = new System.Drawing.Size(121, 21);
+            this.ComboBoxProgressStep.TabIndex = 8;
+            this.ComboBoxProgressStep.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // FrmProgressBarSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 141);
+            this.Controls.Add(this.ComboBoxProgressStep);
+            this.Controls.Add(this.ButtonStop);
+            this.Controls.Add(this.ButtonReset);
+            this.Controls.Add(this.LabelProgressStep);
+            this.Controls.Add(this.LabelStatusText);
+            this.Controls.Add(this.LabelStatus);
+            this.Controls.Add(this.LabelPercentCompleteText);
+            this.Controls.Add(this.LabelPercentComplete);
+            this.Controls.Add(this.ProgressBarProcess);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmProgressBarSample";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "[Allineed.Ru] Пример работы с элементом ProgressBar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar ProgressBarProcess;
+        private System.Windows.Forms.Label LabelPercentComplete;
+        private System.Windows.Forms.Label LabelPercentCompleteText;
+        private System.Windows.Forms.Label LabelStatus;
+        private System.Windows.Forms.Label LabelStatusText;
+        private System.Windows.Forms.Label LabelProgressStep;
+        private System.Windows.Forms.Button ButtonReset;
+        private System.Windows.Forms.Button ButtonStop;
+        private System.Windows.Forms.Timer TimerForProgressBar;
+        private System.Windows.Forms.ComboBox ComboBoxProgressStep;
+    }
+}
+
---
+++ ProgressBar/WindowsFormsApp2/FrmProgressBarSample.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class FrmProgressBarSample : Form
+    {
+        class ProgressStep
+        {
+            public string StepText { get; set; }
+            public int StepValue { get; set; }
+            public ProgressStep(string stepText, int stepValue)
+            {
+                StepText = stepText;
+                StepValue = stepValue;
+            }
+            public override string ToString()
+            {
+                return StepText;
+            }
+        }
+
+        public FrmProgressBarSample()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateProgressBarPercentComplete();
+            FillProgressSteps();
+        }
+
+       
+             private void FillProgressSteps()
+        {
+            ComboBoxProgressStep.Items.Add(new ProgressStep("10%", 10));
+            ComboBoxProgressStep.Items.Add(new ProgressStep("20%", 20));
+            ComboBoxProgressStep.Items.Add(new ProgressStep("30%", 30));
+            ComboBoxProgressStep.Items.Add(new ProgressStep("40%", 40));
+            ComboBoxProgressStep.Items.Add(new ProgressStep("50%", 50));
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                ProgressBarProcess.Value = 0;
+                UpdateProgressBarPercentComplete();
+                if (!TimerForProgressBar.Enabled)
+                {
+                    TimerForProgressBar.Start();
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            {
+                if (ComboBoxProgressStep.SelectedItem != null)
+                {
+                    ProgressStep step =
+                   (ProgressStep)ComboBoxProgressStep.SelectedItem;
+                    ProgressBarProcess.Step = step.StepValue;
+                }
+            }
+        }
+
+        private void ProgressBarProcess_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelPercentComplete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelPercentCompleteText_Click(object sender, EventArgs e)
+        { }
+            private void UpdateProgressBarPercentComplete()
+        {
+            LabelPercentCompleteText.Text = ProgressBarProcess.Value + "%";
+            if (ProgressBarProcess.Value >= 0 && ProgressBarProcess.Value < 20)
+            {
+                LabelStatusText.Text = "Инициализация...";
+            }
+            else if (ProgressBarProcess.Value >= 20 && ProgressBarProcess.Value <
+           40)
+            {
+                LabelStatusText.Text = "Подготовка примера...";
+            }
+            else if (ProgressBarProcess.Value >= 40 && ProgressBarProcess.Value <
+           60)
+            {
+                LabelStatusText.Text = "Выполнение задачи #1...";
+            }
+            else if (ProgressBarProcess.Value >= 60 && ProgressBarProcess.Value <
+           80)
+            {
+                LabelStatusText.Text = "Выполнение задачи #2...";
+            }
+            else if (ProgressBarProcess.Value >= 80 && ProgressBarProcess.Value <
+           100)
+            {
+                LabelStatusText.Text = "Почти готово, завершение...";
+            }
+            else
+            {
+                LabelStatusText.Text = "Выполнено.";
+            }
+        }
+        
+
+        private void LabelStatusText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            {
+                if (TimerForProgressBar.Enabled)
+                {
+                    TimerForProgressBar.Stop();
+                    ButtonStop.Text = "&Продолжить";
+                }
+                else
+                {
+                    TimerForProgressBar.Start();
+                    ButtonStop.Text = "&Остановить";
+                }
+            }
+        }
+
+        private void TimerForProgressBar_Tick(object sender, EventArgs e)
+        {
+            {
+                ProgressBarProcess.PerformStep();
+                UpdateProgressBarPercentComplete();
+                ButtonStop.Enabled = ProgressBarProcess.Value != 100;
+                if (ProgressBarProcess.Value >= 100)
+                {
+                    TimerForProgressBar.Stop();
+                }
+            }
+        }
+    }
+}
